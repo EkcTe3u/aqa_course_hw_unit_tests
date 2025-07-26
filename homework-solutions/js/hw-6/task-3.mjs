48a24668e923b403ee30dd3be0116935fd0cc334
Andrey <andreysozdatelev@gmail.com>
2025-07-27 01:00:51 +0300

    hm-6 task-3

--- homework-solutions/js/hw-6/task-3.mjs
+++ homework-solutions/js/hw-6/task-3.mjs
@@ -6,6 +6,19 @@
 
   Присвойте результат в переменную "unique"
 */
+
 let unique;
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 4, 6, 8, 10, 1, 3, 5, 7, 9];
+let arraySorted = array.sort((a, b) => a - b);
+function deleteDublicates(arrayToSort) {
+  let uniqueArray = [];
+  for (let i = 0; i < arraySorted.length; i++) {
+    if (arraySorted[i] === arraySorted[i + 1]) {
+      uniqueArray.push(arraySorted[i]);
+    }
+  }
+  return uniqueArray;
+}
+unique = deleteDublicates(arraySorted);
 
 export { unique };
