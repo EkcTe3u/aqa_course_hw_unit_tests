f42687ec6379f856daff3afc8b909d00f43ea3ec
Andrey <andreysozdatelev@gmail.com>
2025-07-26 14:03:28 +0300

    HW-4

--- homework-solutions/js/hw-4/task-2.js
+++ homework-solutions/js/hw-4/task-2.js
@@ -14,3 +14,28 @@
   5. Проверить задание со следующими значениями в переменной age: 10, 17, 18, 19, 59, 60, 61
 
   */
+
+const minAge = 18;
+const maxAge = 60;
+
+function checkAge(age, name = 'Andrey') {
+  if (typeof age === 'string') {
+    console.log(`Incorrect data type`);
+  } else {
+    if (age < minAge) {
+      console.log(`You don't have access cause your age is ${age}. It's less then 18 :( `);
+    } else if (age >= minAge && age < maxAge) {
+      console.log(`Welcome ${name}!`);
+    } else {
+      console.log(`Technical work`);
+    }
+  }
+}
+checkAge('10');
+checkAge(10);
+checkAge(17);
+checkAge(18);
+checkAge(19, 'Anatoly');
+checkAge(59);
+checkAge(60);
+checkAge(61);
