3b257dc75ff9153a7b8f0066f544e23e4d584f80
Andrey <andreysozdatelev@gmail.com>
2025-07-27 20:49:44 +0300

    HW-7 task-1

--- homework-solutions/js/hw-7/task-1.mjs
+++ homework-solutions/js/hw-7/task-1.mjs
@@ -5,18 +5,46 @@
   - Например: mergeArrays([1,2], [3,4], [5,6]) // [1,2,3,4,5,6]
   - Решить с использованием Spread operator
 */
-function mergeArrays() {
-  // Ваш код
+const firstArray = [1, 2];
+const secondArray = [3, 4];
+const thirdArray = [5, 6];
+let mergeResult;
+function mergeArrays(...arg) {
+  mergeResult = [].concat(...arg);
+  return mergeResult;
 }
+
 /*
   2. Devide by _
     - Написать функцию, которая преобразует любое предложение в вот_Такой_Вот_Вид и возвращает его. 
     - Первое слово должно начинаться с буквы в нижнем регистре, у остальных -  верхнем. 
     - Пример: I am super engineer => i_Am_Super_Engineer
   */
+const sentence = 'I am super engineer';
 function devideBy(sentence) {
-  // Ваш код
+  let replaceSentence = sentence.replace(/\s+/g, ' ').trim();
+  let splitedSentence = replaceSentence.split(' ');
+  let allLowCase = [];
+  let newCaseSentence = [];
+
+  splitedSentence.forEach((element) => {
+    element = element.toLowerCase();
+    allLowCase.push(element);
+  });
+
+  allLowCase.forEach((element, index) => {
+    if (index === 0) {
+      element = element.charAt(0).toLowerCase() + element.slice(1);
+      newCaseSentence.push(element);
+    } else {
+      element = element.charAt(0).toUpperCase() + element.slice(1);
+      newCaseSentence.push(element);
+    }
+  });
+  const resultSentence = newCaseSentence.join('_');
+  return resultSentence;
 }
+
 /*
   3. Фибаначчи
     - Напишите функцию fibonacci(n), возвращающую энное число Фибоначчи
@@ -26,7 +54,12 @@ function devideBy(sentence) {
     - Например fibonacci(8) //21
   */
 function fibonacci(n) {
-  // Ваш код
+  let array = [0, 1];
+  for (let i = 0; i < n; i++) {
+    let newIndex = array[i] + array[i + 1];
+    array.push(newIndex);
+  }
+  return array[n];
 }
 
 export { mergeArrays, fibonacci, devideBy };
