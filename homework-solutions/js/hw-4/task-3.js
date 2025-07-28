66eb1243110ce2b93b12d9ff0466d63e1eb6eb84
Andrey <andreysozdatelev@gmail.com>
2025-07-28 22:53:51 +0300

    fix with comment HW-4 task-3

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -9,7 +9,10 @@
 const minAge = 18;
 const maxAge = 60;
 function checkAge(age, name = 'Andrey') {
-  if (typeof age === 'number') {
+  if (isNaN(age) || age === null) {
+    console.log(`Incorrect data type`);
+  } else {
+    age = Number(age);
     if (age < minAge) {
       console.log(`You don't have access cause your age is ${age}. It's less then 18 :( `);
     } else if (age >= minAge && age <= maxAge) {
@@ -19,21 +22,6 @@ function checkAge(age, name = 'Andrey') {
     } else {
       console.log(`Technical work`);
     }
-  } else {
-    if (isNaN(age) || age === null) {
-      console.log(`Incorrect data type`);
-    } else {
-      age = Number(age);
-      if (age < minAge) {
-        console.log(`You don't have access cause your age is ${age}. It's less then 18 :( `);
-      } else if (age >= minAge && age <= maxAge) {
-        console.log(`Welcome ${name}!`);
-      } else if (age > maxAge) {
-        console.log(`Keep calm and look Culture channel`);
-      } else {
-        console.log(`Technical work`);
-      }
-    }
   }
 }
 
@@ -41,11 +29,11 @@ checkAge(12);
 checkAge('55', 'Anatoly');
 checkAge('12fg');
 checkAge('fg');
-// checkAge(undefined);
-// checkAge(NaN);
-// checkAge(null);
+checkAge(undefined);
+checkAge(NaN);
+checkAge(null);
 // checkAge(18);
-// checkAge(19, 'Anatoly');
+checkAge(19, 'Anatoly');
 // checkAge(59);
 // checkAge(60);
-// checkAge(150);
+checkAge(150);
